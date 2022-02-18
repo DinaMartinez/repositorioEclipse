5744448da4ac91cc16e952dc64179803bf2d074c
Dina <dinaalmeria@gmail.com>
2022-02-18 10:38:31 +0100

    validamos prueba1

---
+++ Prueba1/src/Prueba1/Vector.java
@@ -0,0 +1,13 @@
+package Prueba1;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		//TODO Esbozo de método generado automáticamente
+		int n=5, result=0;
+		int a[]=new int[5];
+		
+		
+	}
+
+}
