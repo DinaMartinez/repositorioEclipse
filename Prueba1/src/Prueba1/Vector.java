25c2c7741cb89d077b7440751718fd3761f50173
Dina <dinaalmeria@gmail.com>
2022-02-18 13:46:44 +0100

    modificacoin de la variable n desde local

--- Prueba1/src/Prueba1/Vector.java
+++ Prueba1/src/Prueba1/Vector.java
@@ -5,9 +5,7 @@ public class Vector {
 	public static void main(String[] args) {
 		//TODO Esbozo de método generado automáticamente
 		int n=5, result=0;
-		int a[]=new int[5];
-		a[0]= 1;
-		a[1] = 1;
+		int a[]=new int[n];
 	}
 
 }
