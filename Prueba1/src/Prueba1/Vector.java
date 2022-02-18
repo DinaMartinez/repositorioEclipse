c3b7e51d89736a84b710658c07b4cd8e80cdbe4b
DinaMartinez <98164900+DinaMartinez@users.noreply.github.com>
2022-02-18 13:36:51 +0100

    modificacion de la variable n

--- Prueba1/src/Prueba1/Vector.java
+++ Prueba1/src/Prueba1/Vector.java
@@ -3,9 +3,9 @@ package Prueba1;
 public class Vector {
 
 	public static void main(String[] args) {
-		//TODO Esbozo de mÈtodo generado autom·ticamente
+		//TODO Esbozo de m√©todo generado autom√°ticamente
 		int n=5, result=0;
-		int a[]=new int[5];
+		int a[]=new int[n];
 		for(int i= 0; i<a.length; i++)
 		{
 			a[i]=(i+1)*10;
